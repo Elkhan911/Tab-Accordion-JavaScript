934d83f59302a52023b95671a4e54b73ad27de62
Elkhan911 <955pro@mail.ru>
2022-08-25 21:02:21 +0500

    variables for js

---
+++ script.js
@@ -0,0 +1,9 @@
+const link1 = document.querySelector("#_link1");
+const link2 = document.querySelector("#_link2");
+const link3 = document.querySelector("#_link3");
+
+const content1 = document.querySelector("#_content1");
+const content2 = document.querySelector("#_content2");
+const content3 = document.querySelector("#_content3");
+
+link1.addEventListener("click", function () {});
