630bd856fafd8083a8fca6e7775c72a6abd961bb
Elkhan911 <955pro@mail.ru>
2022-08-25 23:41:20 +0500

    fix minors js bags

--- script.js
+++ script.js
@@ -8,58 +8,43 @@ const content2 = document.querySelector("#_content2");
 const content3 = document.querySelector("#_content3");
 const content4 = document.querySelector("#_content4");
 
+// меняем класс двум элементам на active
+function toggleClass(element1, class1, element2, class2) {
+  element1.classList.toggle(class1);
+  element2.classList.toggle(class2);
+}
+
+// удаляем трем элементам общий класс
+function deleteActive(elem1, elem2, elem3, commonClass) {
+  elem1.classList.remove(commonClass);
+  elem2.classList.remove(commonClass);
+  elem3.classList.remove(commonClass);
+}
+
 link1.addEventListener("click", function () {
-  toggleClass(content1, "tabs__content_active");
-  toggleClass(link1, "tabs__link_active");
+  toggleClass(content1, "tabs__content_active", link1, "tabs__link_active");
 
-  content2.classList.remove("tabs__content_active");
-  content3.classList.remove("tabs__content_active");
-  content4.classList.remove("tabs__content_active");
-
-  link2.classList.remove("tabs__link_active");
-  link3.classList.remove("tabs__link_active");
-  link4.classList.remove("tabs__link_active");
+  deleteActive(content2, content3, content4, "tabs__content_active");
+  deleteActive(link2, link3, link4, "tabs__link_active");
 });
 
 link2.addEventListener("click", function () {
-  toggleClass(content2, "tabs__content_active");
-  toggleClass(link2, "tabs__link_active");
+  toggleClass(content2, "tabs__content_active", link2, "tabs__link_active");
 
-  content1.classList.remove("tabs__content_active");
-  content3.classList.remove("tabs__content_active");
-  content4.classList.remove("tabs__content_active");
-
-  link1.classList.remove("tabs__link_active");
-  link3.classList.remove("tabs__link_active");
-  link4.classList.remove("tabs__link_active");
+  deleteActive(content1, content3, content4, "tabs__content_active");
+  deleteActive(link1, link3, link4, "tabs__link_active");
 });
 
 link3.addEventListener("click", function () {
-  toggleClass(content3, "tabs__content_active");
-  toggleClass(content3, "tabs__link_active");
+  toggleClass(content3, "tabs__content_active", link3, "tabs__link_active");
 
-  content1.classList.remove("tabs__content_active");
-  content2.classList.remove("tabs__content_active");
-  content4.classList.remove("tabs__content_active");
-
-  link1.classList.remove("tabs__link_active");
-  link2.classList.remove("tabs__link_active");
-  link4.classList.remove("tabs__link_active");
+  deleteActive(content1, content2, content4, "tabs__content_active");
+  deleteActive(link1, link2, link4, "tabs__link_active");
 });
 
 link4.addEventListener("click", function () {
-  toggleClass(content4, "tabs__content_active");
-  toggleClass(content4, "tabs__link_active");
+  toggleClass(content4, "tabs__content_active", link4, "tabs__link_active");
 
-  content1.classList.remove("tabs__content_active");
-  content2.classList.remove("tabs__content_active");
-  content3.classList.remove("tabs__content_active");
-
-  link1.classList.remove("tabs__link_active");
-  link2.classList.remove("tabs__link_active");
-  link3.classList.remove("tabs__link_active");
+  deleteActive(content1, content2, content3, "tabs__content_active");
+  deleteActive(link1, link2, link3, "tabs__link_active");
 });
-
-function toggleClass(elem, newClass) {
-  elem.classList.toggle(newClass);
-}
